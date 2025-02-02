b5d4e27a57dee285dbd8b642eb7189d168a391e3
xsaven <gms.sw.xsaven@gmail.com>
2025-02-02 09:28:24 +0200

    .cli/cfg/set.js

--- .cli/cfg/set.js
+++ .cli/cfg/set.js
@@ -8,6 +8,11 @@ module.exports = class Set extends process.Command {
         ['-s, --store <store>', 'Set store for config save', 'tmp'],
     ];
 
+    commandDocumentation = `If you want to set a value from environment variables, you can use the following syntax:
+    ${`cli cfg:set my_name "\\\${env.USER}"`.green}
+Or with a default value:
+    ${`cli cfg:set my_name "\\\${env.USER || John Doe}"`.green}`;
+
     options = {
         store: 'tmp'
     };
